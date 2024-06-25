8e34f340ca59b590fcedb8b493e6e137d33b5633
Vadym Bakuma <bakuma.2001@gmail.com>
2024-06-25 09:16:43 +0200

    make build

---
+++ index.7889cffd.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.clientWidth/2,l=e.clientHeight/2;t.style.left="".concat(c-t.offsetWidth/2,"px"),t.style.top="".concat(l-t.offsetHeight/2,"px");
+//# sourceMappingURL=index.7889cffd.js.map
